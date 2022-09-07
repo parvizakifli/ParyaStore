56f404396e88c7e52bb1c9d86d862c23ebd70ab9
Parviz <parviz.akifli@mail.ru>
2022-09-07 23:16:43 +0400

    yeaahh

--- src/App.js
+++ src/App.js
@@ -1,9 +1,1 @@
-import React from "react";
-import { Header } from "./components/Header/Header";
-import { Main } from "./screesns/mainScrens/Main";
-export const App=()=>{
-  return(<>
-  <Header/>
-  <Main/>
-  </>)
-}
+import Re
