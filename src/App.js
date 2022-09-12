05ff300b4f077dc90cd187fb4e793c3d96c72763
Parviz <parviz.akifli@mail.ru>
2022-09-13 00:13:03 +0400

    fix:taskname:fix all bugs

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React from "react";
-import logo from "./logo.svg";
+import { Routes, Route } from "react-router-dom";
+import { Items } from "screens/items";
+import { Categories } from "screens/categories";
+export const App = () => {
+  return <>
+    <Routes>
+      <Route path="/" element={<Items />} />
+      <Route path="categories" element={<Categories />} />
+    </Routes>
+  </>;
+};
 
-
-function App() {
- 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
